1694d4fb8dbf568c1f8271ad4beb8cc185971003
Vipul Sharma <vipul7008@gmail.com>
2025-06-22 18:45:34 +0530

    new project quiz game added under day 17

---
+++ Day 17/practise/main.py
@@ -0,0 +1,15 @@
+class User:
+    def __init__(self, user_id, user_name):
+        self.id = user_id
+        self.username = user_name
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
+
+user_1 = User("001", "Vipul")
+
+print(user_1.id)
